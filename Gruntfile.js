ab096915526ea4f786f69bfffd27bc5029eb64d0
MatthewGeorge <matt.george14@gmail.com>
2014-06-06 05:56:46 +0000

    changed host in Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -64,9 +64,9 @@ module.exports = function (grunt) {
     // The actual grunt server settings
     connect: {
       options: {
-        port: 9000,
-        // Change this to '0.0.0.0' to access the server from outside.
-        hostname: 'localhost',
+        port: 3000,
+        // Change this to 'localhost' to access the server locally.
+        hostname: '0.0.0.0',
         livereload: 35729
       },
       livereload: {
