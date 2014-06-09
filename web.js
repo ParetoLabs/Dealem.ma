cd6255a0abd09b064e4c3f20079985a34e9ae21b
Jisan Zaman <jzthegreat@gmail.com>
2014-06-09 13:45:51 -0400

    build

--- web.js
+++ web.js
@@ -4,8 +4,8 @@ var gzippo = require('gzippo');
 var logger = require('morgan')
 
 var app = express();
-app.use(logger());
-app.use(gzippo.staticGzip('' + __dirname));
+app.use(logger('dev'));
+app.use(gzippo.staticGzip('' + __dirname + "/dist"));
 
 var server = http.createServer(app);
 server.listen(process.env.PORT || 5000);
